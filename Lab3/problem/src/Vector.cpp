9853dc64b2dd4a59b77083b44f5552b89591cb72
Felix Hong <hongf@wiliki.eng.hawaii.edu>
2018-02-06 18:07:12 -1000

    Finished Vector.cpp

--- Lab3/problem/src/Vector.cpp
+++ Lab3/problem/src/Vector.cpp
@@ -1,50 +1,86 @@
 #include "Vector.hpp"
-
 #include <utility>
 
+//Standard default constructor
 Vector::Vector():
 	arr(new int[1]),
 	length(0),
 	capacity(1) {}
 
+//copy operator
 Vector::Vector(const Vector& other):
 	arr(new int[other.size()]),
 	length(other.size()),
-	capacity(other.capacity()) {
+	capacity(other.capacity) {
 	for(std::size_t i=0; i < other.size(); i++){
 		this->arr[i] = other.arr[i];}}
 
+//Move operator
 Vector::Vector(Vector&& other):
-	arr(0),
-	length(0),
-	capacity(0) {}
+	arr(std::move(other.arr)),
+	length(std::move(other.length)),
+	capacity(std::move(other.capacity)){}
 
-void append(int num){
-	if(capacity(other.capacity())
-		throw const char*
+
+void Vector::append(int num){
+	if(capacity < this->length+1) {
+			throw "the index is out of bounds";
+	}
+	this->arr[length+1] = num;
+}
 	
-
-
-void insert(int index, int num){
-	if(out)
-		throw const char*
-	this->length
-
-void remove(int index){
-	if(out)
-		throw const char*
-	get number at index
-
-int get(int index) const{
-	return length;
-
-std::size_t size() const{
-	return ref;
-
-int& operator[](int index){
-	 
-
-Vector::~Vector(){
-	delete arr[];
+void Vector::insert(int index, int num){// over write with instructors!!!
+	if(index < 0 || index > capacity) {
+		throw "the index is out of bounds";
+	}
+	else if ((length+1) > capacity ) {
+		this->double_capacity();
+	}
+	for(std::size_t i=index;i <= length-1;i++) {
+		this->arr[i+1] = arr[i];
+	}
+	arr[index] = num;
+	this->length++;
 }
 
+void Vector::remove(int index){
+	if(index < 0 || index > length - 1) {
+		throw "the index is out of bounds";
+	}
+	for(std::size_t i=index;i <= length-1;i++) {
+		this->arr[i] = arr[i+1];
+	}
+	this->length--;
+}
+	
+int Vector::get(int index) const{
+	if(index < 0 || index >= capacity ) {
+		throw "the index is out of bounds";
+	}
+	return this->arr[index];
+}
+
+std::size_t Vector::size() const{
+	return this->length;
+}
+	
+int& Vector::operator[](int index){
+	if(index < 0 || index >= capacity ) {
+		throw "the index is out of bounds";
+	}
+	return this->arr[index];
+}
+
+Vector::~Vector(){
+	delete [] arr;
+}
+
+void Vector::double_capacity(){
+	int *temp_arr = new int[this->capacity*2];
+	for(std::size_t i=0;i <= this->length-1;i++) {
+		temp_arr[i] = arr[i];
+	}
+	delete [] arr;
+	arr = temp_arr;
+	this->capacity=this->capacity*2;
+}
