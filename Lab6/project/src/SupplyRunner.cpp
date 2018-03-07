f7892346c32929a3349a2e60476dc63e16d80a1d
Felix Hong <hongf@wiliki.eng.hawaii.edu>
2018-03-06 17:34:38 -1000

    runner kinda works

--- Lab6/project/src/SupplyRunner.cpp
+++ Lab6/project/src/SupplyRunner.cpp
@@ -1,2 +1,72 @@
 #include "SupplyRunner.hpp"
+
 #include "Storeroom.hpp"
+#include "Ingredient.hpp"
+
+#include <string>
+#include <vector>
+#include <map>
+
+//construct
+SupplyRunner::SupplyRunner(Storeroom& storeroom_) :
+	storeroom(storeroom_) {}
+	
+//Getter
+std::vector<Ingredient> SupplyRunner::get_ingredients(IngredientMap ingredients) {
+	
+	//variables
+	bool foundIngredient = false;
+	
+	//Setup mapping names
+    std::map<Ingredient, unsigned>::iterator it = ingredients.begin();
+	std::map<Ingredient, unsigned>::iterator it1 = storeroom.begin();
+	
+	//go through ingredients
+	while(it != ingredients.end()){
+		//check through the storeroom and quit if ingredent is found
+		while(it1 != storeroom.end() && foundIngredient == false) {
+			if(it->first == it1->first) {
+				if(it->second <= it1->second) {
+					foundIngredient = true;
+				}
+			}
+			it1++;
+		}
+		
+		if(foundIngredient == true) {
+			//if ingredent were found reset 
+			foundIngredient = false;
+			it1 = storeroom.begin();
+		}
+		else {
+			throw "Not enough ingredent or ingredent not found";
+		}
+		it++;
+	}
+	
+	//reset iterator
+	it1 = storeroom.begin();
+	it = ingredients.begin();
+	
+	//Go through again and remove ingredients
+	while(it != ingredients.end()){
+		while(it1 != storeroom.end()) {
+			if(it1->first == it->first) {
+				if(it->second <= it1->second) {
+					it1->second -= it->second;
+				}
+			}
+			it1++;
+		}
+		it++;
+	}
+	
+	std::vector<Ingredient> returnIngredient;
+	while(it != ingredients.end()) {
+		returnIngredient.push_back(it->first);
+		it++;
+	}
+	
+	return returnIngredient;
+}
+	
