c72096da2fc32416bf9adf6b30e31f7790b8dd72
Felix Hong <hongf@wiliki.eng.hawaii.edu>
2018-03-19 21:08:21 -1000

    Working copy

--- Lab6/project/src/Cashier.cpp
+++ Lab6/project/src/Cashier.cpp
@@ -17,8 +17,7 @@ double Cashier::get_money() const {
 	return this->money;
 }
 
-void Cashier::serve_customer(std::queue<Customer>& line, std::stack<Order>& orders) 
-{
+void Cashier::serve_customer(std::queue<Customer>& line, std::stack<Order>& orders) {
 	//Creates variables we will use;
 	std::vector<std::string>  name_of_items;
 	std::string name_of_item;
@@ -32,10 +31,10 @@ void Cashier::serve_customer(std::queue<Customer>& line, std::stack<Order>& orde
 	std::string customer_order = current_customer.get_order();
 	
 	//Convert customer's order into a vector of  name_of_items
-/*	for(unsigned int cnt = 0; cnt < customer_order.size(); cnt++) {
+	for(unsigned int cnt = 0; cnt < customer_order.size(); cnt++) {
 		number_of_items = 0;
 		name_of_item.clear();
-		
+
 		while(customer_order[cnt] != ' ') {
 			//Convert character array into int
 			if((customer_order[cnt] - 48) > 0 || (customer_order[cnt] - 48) <=9) {
@@ -48,7 +47,7 @@ void Cashier::serve_customer(std::queue<Customer>& line, std::stack<Order>& orde
 			cnt++;
 		}
 		cnt++;
-	
+		
 		//Grab customer's order
 		while(customer_order[cnt] != ' ' && customer_order[cnt] != '\0' ) {
 			name_of_item += customer_order[cnt];
@@ -59,9 +58,8 @@ void Cashier::serve_customer(std::queue<Customer>& line, std::stack<Order>& orde
 		for(unsigned int cnt1 = 0; cnt1 < number_of_items ; cnt1++) {
 			name_of_items.push_back(lexical_cast(name_of_item));
 		}
-	}*/
-        name_of_items = split(customer_order, ' ');
-
+	}
+	
 	//Calculate total cost and validates customer's order
 	try {
 		total_cost = calculate_order_cost(name_of_items);
