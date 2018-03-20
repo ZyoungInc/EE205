9d9e5ba646ad885fde4e9b6bbaa43de46c3cb1fd
Felix Hong <hongf@wiliki.eng.hawaii.edu>
2018-03-19 21:56:21 -1000

    protototype of cook.cpp(working maybe?)

--- Lab6/project/src/Cook.cpp
+++ Lab6/project/src/Cook.cpp
@@ -8,17 +8,16 @@
 #include <stack>
 //using RecipeBook = std::map<std::string, std::map<Ingredient, unsigned>>; 
 
-RecipeBook recipes;
-
 Cook::Cook(SupplyRunner& runner_, Kitchen& kitchen_) :
 	runner(runner_),
 	kitchen(kitchen_) {}
-	/*
+	
 void Cook::prepare_dish(
 	std::stack<Order>& orders, 
 	std::queue<std::pair<std::size_t, Dish>>& finished_dishes) {
 		
 	std::vector<std::string> order_list;
+	std::vector<Ingredient> v_supplies_needed;
 	std::map<Ingredient, unsigned> required_ingredientsl;
 	std::pair<std::size_t, Dish> finished_item;
 	
@@ -33,7 +32,32 @@ void Cook::prepare_dish(
 			if(search != recipes.end()) {
 				try {
 					finished_item.first = current_order.get_id();
-					finished_item.second = kitchen.prepare_dish(runner.get_ingredients(search->second));
+					
+					try {
+						v_supplies_needed = runner.get_ingredients(search->second)
+					}
+					catch (char const* c) {
+						throw c;
+					}
+					/*
+					//convert vector to map
+					for(unsigned int cnt = 0; cnt < v_supplies_needed.size(); cnt++) {
+						std::map<Ingredient, unsigned>::iterator it = required_ingredientsl.begin();
+						
+						while(it != required_ingredientsl.end() && found_ingredient == false){
+							if(required_ingredientsl->first == v_supplies_needed [cnt]) {
+								required_ingredientsl->second++;
+								found_ingredient = true;
+							}
+						}
+						if(found_ingredient == false) {
+							required_ingredientsl.insert(std::pair<Ingredient, unsigned>(v_supplies_needed [cnt], 1);
+						}
+						found_ingredient = false;
+					}
+					*/	
+					
+					finished_item.second = kitchen.prepare_dish(search->second);
 					finished_dishes.push(finished_item);
 				}
 				catch (char const* c) {
@@ -47,4 +71,3 @@ void Cook::prepare_dish(
 	}
 }
 
-*/
