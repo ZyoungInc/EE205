69e580c9fdbc65bf3b29579183b42c7343109c2c
Jared <aujared@hawaii.edu>
2018-04-30 02:28:54 -1000

    Adding player files again

--- Final/project/include/player.hpp
+++ Final/project/include/player.hpp
@@ -1,13 +1,41 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include "Unit.hpp"
 
 using namespace std;
 
-class Player : public Unit{
+class Player{
 	private:
+		int health;
+		int MAXhealth;
+		int mana;
+		int MAXmana;
+		int att;
+		int def;
 		pair <string, string> player_eff;
+
 	public:
+		Weapon basicwep;
+		Player();
+		Player(Weapon wep);
+		Player(Player&& other )
 		pair <string, string>  get_player_eff(void);
+
+		//Getters
+		std::string get_name();
+		int get_health();
+		int get_MAXhealth();
+		int get_mana();
+		int get_MAXmana();
+		int get_attack();
+		int get_defense();
+
+		//Setters
+		void set_name(std::string s);
+		void set_health(int hp);
+		void set_MAXhealth(int hp);
+		void set_mana(int mp);
+		void set_MAXmana(int mp);
+		void set_attack(int atk);
+		void set_defense(int d);
 };
--- Final/project/src/player.cpp
+++ Final/project/src/player.cpp
@@ -1,10 +1,66 @@
 #include <string>
 #include <utility>
 #include "game-utility.hpp"
-#include "Unit.hpp"
 #include "player.hpp"
 
 using namespace std;
+
+Weapon basicwep;
+// Constructs a Player with default stats and  chosen name
+player::player(string name):
+  name(name),
+	MAXhealth(basicwep.MAXHealth),
+  MAXmana(basicwep.MAXmana),
+	health(basicwep.MAXHealth),
+	mana(basicwep.MAXmana),
+  att(basicwep.wdamage),
+  def(basicwep.wdefense) {}
+
+  //Move Constructor
+  Player::Player(Player&& other, char rarity_level) :
+    newwep Warrior_Weap(rarity_level);
+    name(move other.name),
+    health(move(other.health)),
+    mana(move(other.mana)),
+    att(newwep.att),
+    def(newwep.def),
+    MAXHealth(newwep.MAXHealth),
+    MAXMana(newwep.MAXMana) {}
+
+    //Getters
+    string get_name() {return name;}
+    int get_health() {return health;}
+    int get_MAXhealth() {return MAXhealth;}
+    int get_mana() {return mana;}
+    int get_MAXmana() {return MAXmana;}
+    int get_attack(); {return attack;}
+    int get_defense() {return defense;}
+
+    //Setters
+    void set_name(string s) {name = s;}
+    void set_health(int hp)
+    if (hp + health > MAXhealth)
+      health = MAXhealth;
+      else if (health + hp < 0)
+      health = 0;
+    else
+          health += hp;
+  }
+    void set_MAXhealth(int hp){Maxhealth = hp;}
+    void set_mana(int mp)
+    if (mp + mana > MAXmana)
+  		mana = MAXmana;
+      else if (mana + mp < 0)
+  		mana = 0;
+  	else
+          mana += mp;
+  }
+    void set_MAXmana(int mp){Maxmana = mp;}
+    void set_attack(int atk){ att = atk;}
+    void set_defense(int d){def = d;}
+
+
+
 
 pair <string, string>  Player::get_player_eff() {
 	std::pair <string, string> temp_player_eff;
