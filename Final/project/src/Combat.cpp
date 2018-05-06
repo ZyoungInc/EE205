1b0cc9ad07cb27db506d7373b4104aaec5e58fa3
Felix Hong <hongf@wiliki.eng.hawaii.edu>
2018-05-05 16:32:35 -1000

    No real changes, cleaned up tabspacing in combat

--- Final/project/src/Combat.cpp
+++ Final/project/src/Combat.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <cstdlib>
 #include <algorithm>
 #include <string>
@@ -13,6 +12,9 @@
 #include "game-utility.hpp"
 #include "Weapon.hpp"
 
+//Defines the size of the combat log
+#define MAX_COMBATLOG_SIZE 7
+
 //Default Constructor
 Combat::Combat() {}
 
@@ -22,12 +24,12 @@ Combat::~Combat() {}
 //Overloaded Constructor
 Combat::Combat(Player *p_, Enemy *e_, int yMax_, int xMax_)
 {
-  p = p_;
-  e = e_;
-  this->yMax = yMax_;
-  this->xMax = xMax_;
+	this->p = p_;
+	this->e = e_;
+	this->yMax = yMax_;
+	this->xMax = xMax_;
 
-  //create title window for game
+	//create title window for game
 	combatwin = newwin(yMax-12, xMax-1, 1, 1);
     box(combatwin, 0, 0);
     mvwprintw(combatwin, get_yshift(), 1, "You encounter a %s", e->get_name().c_str());
@@ -39,134 +41,128 @@ Combat::Combat(Player *p_, Enemy *e_, int yMax_, int xMax_)
 int Combat::combat_phase(unsigned int action, int classPick, Player *p, Enemy *e)
   {
     float effect;
-
         effect = eff_calc_other(p->get_weapType(), p->get_eleType(),
-                                e->get_wep_type(), e->get_element());
+							e->get_wep_type(), e->get_element());
         player_attack(p, e, action, classPick, effect);
     if(e->get_health() > 0)
     {
       effect = eff_calc_other(e->get_wep_type(), e->get_element(),
-                              p->get_weapType(), p->get_eleType());
+							p->get_weapType(), p->get_eleType());
       enemy_attack(p, e, effect);
     }
-	//display combat log
+	
+	//Iterate through combat log and display it on screen
 	int cnt = 0;
 	for(unsigned int i = combat_log.size(); i != 0; i--) {
 		mvwprintw(combatwin, (2+cnt), 3, combat_log[(i-1)].c_str());
 		cnt ++;
 		wrefresh(combatwin);
 	}
-	//mvwprintw(combatwin, get_yshift(), 3, "Number1 : %d" , combat_log.size());
-	//wrefresh(combatwin);
-	
+
     if(e->get_health() == 0)
-      return 1;
+		return 1;
+		//Enemy has died
     else if(p->get_health() == 0)
-      return 2;
+		return 2;
+		//Player has died
     else
-      return 3;
+		return 3;
+		//No one died, continue combat
   }
 
 float Combat::eff_calc_other(std::string atkWeap, std::string atkEle,
                           std::string defWeap, std::string defEle) {
-  float effect = 1;
-  //Element triange
-  if(atkEle == "Fire" && defEle == "Nature") effect = 2;
-  else if(atkEle == "Fire" && defEle == "Water") effect = 0.5;
-  else if(atkEle == "Water" && defEle == "Nature") effect = 2;
-  else if(atkEle == "Water" && defEle == "Nature") effect = 0.5;
-  else if(atkEle == "Nature" && defEle == "Water") effect = 2;
-  else if(atkEle == "Nature" && defEle == "Fire") effect = 0.5;
-  //Weapon Triangle
-  else if(atkWeap == "Sword" && defWeap == "Axe") effect = 2;
-  else if(atkWeap == "Sword" && defWeap == "Lance") effect = 0.5;
-  else if(atkWeap == "Lance" && defWeap == "Sword") effect = 2;
-  else if(atkWeap == "Lance" && defWeap == "Axe") effect = 0.5;
-  else if(atkWeap == "Axe" && defWeap == "Lance") effect = 2;
-  else if(atkWeap == "Axe" && defWeap == "Sword") effect = 0.5;
-  else effect = 1;
+	float effect = 1;
+	//Element triange
+	if(atkEle == "Fire" && defEle == "Nature") effect = 2;
+	else if(atkEle == "Fire" && defEle == "Water") effect = 0.5;
+	else if(atkEle == "Water" && defEle == "Nature") effect = 2;
+	else if(atkEle == "Water" && defEle == "Nature") effect = 0.5;
+	else if(atkEle == "Nature" && defEle == "Water") effect = 2;
+	else if(atkEle == "Nature" && defEle == "Fire") effect = 0.5;
+	
+	//Weapon Triangle
+	else if(atkWeap == "Sword" && defWeap == "Axe") effect = 2;
+	else if(atkWeap == "Sword" && defWeap == "Lance") effect = 0.5;
+	else if(atkWeap == "Lance" && defWeap == "Sword") effect = 2;
+	else if(atkWeap == "Lance" && defWeap == "Axe") effect = 0.5;
+	else if(atkWeap == "Axe" && defWeap == "Lance") effect = 2;
+	else if(atkWeap == "Axe" && defWeap == "Sword") effect = 0.5;
+	else effect = 1;
 
-  return effect;
-
+	return effect;
 }
+
 void Combat::player_attack(Player *p, Enemy *e, unsigned int action, int classPick, float effect) {
-  int damage;
-  if(action == 2)
-  {
-    damage = std::max(0, int(p->get_attack() * effect - e->get_defense()));
-    p->set_mana(20);
-    //testing
-    //mvwprintw(combatwin, get_yshift(), 1, "You dealt %d damage to %s with basic attack", damage, e->get_name().c_str());
-	//std::string temp_log("You dealt %d damage to %s with basic attack", damage, e->get_name().c_str());
-	temp_combat_log.clear();
-	temp_combat_log.append("You dealt ");
-	temp_combat_log.append(lexical_cast(damage));
-	temp_combat_log.append(" damage to ");
-	temp_combat_log.append(e->get_name());
-	temp_combat_log.append(" with basic attack");
-	
-	if(combat_log.size() >= 5) {
-		combat_log.erase(combat_log.begin());
-		combat_log.push_back(temp_combat_log);
+	int damage;
+	if(action == 2)
+	{
+		damage = std::max(0, int(p->get_attack() * effect - e->get_defense()));
+		p->set_mana(20);
+
+		temp_combat_log.clear();
+		temp_combat_log.append("You dealt ");
+		temp_combat_log.append(lexical_cast(damage));
+		temp_combat_log.append(" damage to ");
+		temp_combat_log.append(e->get_name());
+		temp_combat_log.append(" with basic attack");
+
+		if(combat_log.size() >= 5) {
+			combat_log.erase(combat_log.begin());
+			combat_log.push_back(temp_combat_log);
+		}
+		else {
+			combat_log.push_back(temp_combat_log);
+		}
+	}
+	else if(action == 7) {
+		//Flee command
+		if(combat_log.size() == MAX_COMBATLOG_SIZE) {
+			combat_log.erase(combat_log.begin());
+			combat_log.push_back("You've attempted to flee");
+		}
+		else {
+			combat_log.push_back("You've attempted to flee");
+		}
+		damage = 0;
 	}
 	else {
-		combat_log.push_back(temp_combat_log);
-	}
+		std::pair<int, int> spellDamage = p->cast_spell(classPick, action-3, p->get_attack());
+		damage = std::max(0, spellDamage.second * int(spellDamage.first * effect - e->get_defense()));
+		
+		temp_combat_log.clear();
+		temp_combat_log.append("You dealt ");
+		temp_combat_log.append(lexical_cast(damage));
+		temp_combat_log.append(" damage to ");
+		temp_combat_log.append(e->get_name());
+		temp_combat_log.append(" with ");
+		temp_combat_log.append(p->get_spellName());
 	
-  }
-  else if(action == 7) {
-	//Flee command
-	if(combat_log.size() == 5) {
-		combat_log.erase(combat_log.begin());
-		combat_log.push_back("You've attempted to flee");
+		if(combat_log.size() >= MAX_COMBATLOG_SIZE) {
+			combat_log.erase(combat_log.begin());
+			combat_log.push_back(temp_combat_log);
+		}
+		else {
+			combat_log.push_back(temp_combat_log);
+		}
 	}
-	else {
-		combat_log.push_back("You've attempted to flee");
-	}
-    damage = 0;
-  }
-  else
-  {
-    std::pair<int, int> spellDamage = p->cast_spell(classPick, action-3, p->get_attack());
-    damage = std::max(0, spellDamage.second * int(spellDamage.first * effect - e->get_defense()));
-    //mvwprintw(combatwin, get_yshift(), 1, "You dealt %d damage to %s with %s", damage, e->get_name().c_str(), p->get_spellName().c_str());
-    //set_yshift(1);
-    //wrefresh(combatwin);
-	temp_combat_log.clear();
-	temp_combat_log.append("You dealt ");
-	temp_combat_log.append(lexical_cast(damage));
-	temp_combat_log.append(" damage to ");
-	temp_combat_log.append(e->get_name());
-	temp_combat_log.append(" with ");
-	temp_combat_log.append(p->get_spellName());
-	
-	if(combat_log.size() >= 5) {
-		combat_log.erase(combat_log.begin());
-		combat_log.push_back(temp_combat_log);
-	}
-	else {
-		combat_log.push_back(temp_combat_log);
-	}
-  }
   
-  e->set_health(-1 * damage);
-  if (e->get_health() <= 0) {
-    on_death();
-  }
+	e->set_health(-1 * damage);
+	if (e->get_health() <= 0) {
+		on_death();
+	}
 }
 
 void Combat::enemy_attack(Player *p, Enemy *e, float effect){
-  int damage = std::max(0, int(e->get_attack() * effect - p->get_defense()));
-  //mvwprintw(combatwin, get_yshift(), 1, "%s dealt %d damage to you", e->get_name().c_str(), damage);
-  //set_yshift(1);
-  //wrefresh(combatwin);
+	int damage = std::max(0, int(e->get_attack() * effect - p->get_defense()));
+  
 	temp_combat_log.clear();
 	temp_combat_log.append(e->get_name());
 	temp_combat_log.append(" dealt ");
 	temp_combat_log.append(lexical_cast(damage));
 	temp_combat_log.append(" damage to you ");
 
-	if(combat_log.size() >= 5) {
+	if(combat_log.size() >= MAX_COMBATLOG_SIZE) {
 		combat_log.erase(combat_log.begin());
 		combat_log.push_back(temp_combat_log);
 	}
@@ -174,25 +170,25 @@ void Combat::enemy_attack(Player *p, Enemy *e, float effect){
 		combat_log.push_back(temp_combat_log);
 	}
   
-  p->set_health(-1 * damage);
-  if (p->get_health() <= 0) {
-    on_death();
-  }
+	p->set_health(-1 * damage);
+	if (p->get_health() <= 0) {
+		on_death();
+	}
 }
 
 bool Combat::is_dead()  {
-  return true;
+	return true;
 }
 
 void Combat::on_death(){
-  is_dead();
+	is_dead();
 
-  //Display Unit died
-  //dialogue.set_yshift(dialogue.updateDeath(this->name));
-  //if player unit is dead, end Game
+	//Display Unit died
+	//dialogue.set_yshift(dialogue.updateDeath(this->name));
+	//if player unit is dead, end Game
 
-  //counter for enemies_killed
-  //set_souls(1);
+	//counter for enemies_killed
+	//set_souls(1);
 }
 
 //Getters
@@ -202,11 +198,11 @@ int Combat::get_yshift() {return yshift;}
 //setters
 void Combat::set_action(int a) {action = a;}
 void Combat::set_yshift(int y) {
-  yshift += y;
-  if(yshift == yMax-12)
-  {
-    wclear(combatwin);
-    box(combatwin, 0, 0);
-    yshift = 1;
-  }
+	yshift += y;
+	if(yshift == yMax-12)
+	{
+		wclear(combatwin);
+		box(combatwin, 0, 0);
+		yshift = 1;
+	}
 }
