cd416bb2e4e1b2f7983c67f3f8f0fb851b6ea620
Chris <christopher.won.hi@gmail.com>
2018-05-09 16:58:31 -1000

    Color def for ncurses

--- Final/project/include/displayColor.hpp
+++ Final/project/include/displayColor.hpp
@@ -1,6 +1,7 @@
 #ifndef DISPLAYCOLOR_HPP
 #define DISPLAYCOLOR_HPP
 
+//Color pair definitions for ncurses to be used in the windows
 #define REDW init_pair(1, COLOR_RED, COLOR_WHITE)
 
 #endif //DISPLAYCOLOR_HPP
